5923537692d4cf5649a6cf31b2dba78bc4208ee0
A-Shermatov <azamatjon.shermatov@gmail.com>
2024-12-07 01:27:32 +0300

    added an extension property and function to StudentProgress object

--- app/src/main/java/com/example/generics_objects_and_extensions/MainActivity.kt
+++ app/src/main/java/com/example/generics_objects_and_extensions/MainActivity.kt
@@ -13,9 +13,9 @@ enum class Difficulty {
 }
 
 class Quiz {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
-    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
-    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+    val question1 = Question("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question("How many days are there between full moons?", 28, Difficulty.HARD)
 
     companion object StudentProgress {
         var total: Int = 10
@@ -24,11 +24,20 @@ class Quiz {
 }
 
 
-fun main() {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
-    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
-    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
-    println(question1.toString())
+val Quiz.StudentProgress.progressText: String
+    get() = "$answered of $total answered"
 
+
+fun Quiz.StudentProgress.printProgressBar() {
+    repeat(answered) { print("▓") }
+    repeat(total - answered) { print("▒") }
+    println()
+    println(Quiz.progressText)
+}
+
+
+fun main() {
     println("${Quiz.answered} of ${Quiz.total} answered.")
+    println(Quiz.progressText)
+    Quiz.printProgressBar()
 }
