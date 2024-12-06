baabc29d3c80ef222430c9ecc3c27b207f586d31
A-Shermatov <azamatjon.shermatov@gmail.com>
2024-12-07 01:11:45 +0300

    added enum class Difficulty

--- app/src/main/java/com/example/generics_objects_and_extensions/MainActivity.kt
+++ app/src/main/java/com/example/generics_objects_and_extensions/MainActivity.kt
@@ -1,27 +1,20 @@
 package com.example.generics_objects_and_extensions
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.generics_objects_and_extensions.ui.theme.Generics_objects_and_extensionsTheme
 
 class Question<T>(
     val questionText: String,
     val answer: T,
-    val difficulty: String
+    val difficulty: Difficulty
 )
 
 
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+
+
 fun main() {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
-    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
-    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
 }
