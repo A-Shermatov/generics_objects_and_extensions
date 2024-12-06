72b84ada5efca1365b374981852fa4736b1afc76
A-Shermatov <azamatjon.shermatov@gmail.com>
2024-12-07 01:45:43 +0300

    final commit

--- app/src/main/java/com/example/generics_objects_and_extensions/MainActivity.kt
+++ app/src/main/java/com/example/generics_objects_and_extensions/MainActivity.kt
@@ -20,9 +20,9 @@ interface ProgressPrintable {
 
 
 class Quiz: ProgressPrintable {
-    val question1 = Question("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
-    val question2 = Question("The sky is green. True or false", false, Difficulty.EASY)
-    val question3 = Question("How many days are there between full moons?", 28, Difficulty.HARD)
+    private val question1 = Question("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    private val question2 = Question("The sky is green. True or false", false, Difficulty.EASY)
+    private val question3 = Question("How many days are there between full moons?", 28, Difficulty.HARD)
 
     companion object StudentProgress {
         var total: Int = 10
