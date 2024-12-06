9b6f73a28cb58356ba4a3519a1a569257758f2ae
A-Shermatov <azamatjon.shermatov@gmail.com>
2024-12-07 01:06:06 +0300

    changed classes to class with generic type

--- app/src/main/java/com/example/generics_objects_and_extensions/MainActivity.kt
+++ app/src/main/java/com/example/generics_objects_and_extensions/MainActivity.kt
@@ -13,20 +13,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.generics_objects_and_extensions.ui.theme.Generics_objects_and_extensionsTheme
 
-class FillInTheBlankQuestion(
+class Question<T>(
     val questionText: String,
-    val answer: String,
+    val answer: T,
     val difficulty: String
 )
 
-class TrueOrFalseQuestion(
-    val questionText: String,
-    val answer: Boolean,
-    val difficulty: String
-)
 
-class NumericQuestion(
-    val questionText: String,
-    val answer: Int,
-    val difficulty: String
-)
+fun main() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
+    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
+    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+}
